711565baee09db9b820130b0f870326132e70af8
antpb <itartist.pdf@gmail.com>
2023-01-25 22:18:17 -0600

    initialize repo

--- README.md
+++ README.md
@@ -1,43 +1,24 @@
-# Discord bot starter template (NodeJS)
+# Pixel Bot (NodeJS)
 
 ## Pre-requisites
 
 - [Node 12 or newer](https://nodejs.org/en/) (LTS recommended)
 - [VS Code](https://code.visualstudio.com/download) (or similar)
-- [Discord Server](https://support.discord.com/hc/en-us/articles/204849977-How-do-I-create-a-server-)
 
-## Quick start
-
-> Go to Discord Apps Dashboard to create your new application or bot: https://discord.com/developers/applications/
-
-1.  Create a `New Application`
-2.  Setup an app name and click on `Create`
-3.  Go to `Bot` settings and add a new one (here you can find the `bot token`)
-4.  Go to `OAuth2` set the stope to `bot`
-5.  Set the bot permissions to `Administrator`
-6.  Copy the scope invitation link and paste on a new tab. Then invite the bot to your server
-
-## Before starting the project
-
-Run the following command:
-
-```sh
-npm init # to setup your package
-npm i discord.js # to install a Discord package and interact with their API
+```npm install
 cp config.mock.json config.json # to setup the your private bot token
 ```
 
-Then update the `BOT_TOKEN` with your real one in the `config.js` file.
+Then update the `BOT_TOKEN` and `OPEN_AI_KEY` with your real one in the `config.js` file.
 
 ```json
 {
-  "BOT_TOKEN": "YOUR_BOT_TOKEN_HERE"
+  "BOT_TOKEN": "YOUR_BOT_TOKEN_HERE",
+  "OPEN_AI_KEY": "YOUR_API_KEY"
 }
 ```
 
-> You can reveal your private bot token in the `Bot` dashboard settings
-
-## Run your project
+## Run the bot
 
 ```sh
 node index.js
--- index.js
+++ index.js
@@ -1,27 +1,71 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits, Intents } = require('discord.js')
 const config = require("./config.json");
+const fetch = require('node-fetch');
 
-const client = new Discord.Client();
+const client = new Client({
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+	intents: ['DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILDS']
+});
 
-const prefix = "!";
+const prefix = '/pixel';
+const endpoint = 'https://alchemy.sxpdigital.workers.dev/';
 
-client.on("message", (message) => {
-  if (message.author.bot) {
-    return;
-  }
+client.on('ready', () => {
+    console.log(`Bot is ready and connected as ${client.user.username}`);
+});
 
-  if (!message.content.startsWith(prefix)) {
-    return;
-  }
+client.on('messageCreate', async message => {
+	console.log(message.content);
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const input = message.content.slice(prefix.length);
 
-  const commandBody = message.content.slice(prefix.length); // command without prefix
-  const args = commandBody.split(" "); // command and args
-  const command = args.shift().toLowerCase(); // command itself
+    // Send the message to the localhost endpoint
+    const client = 1;
+    const channelId = "three";
+    const entity = 1;
+    const speaker = message.author.username;
+    const agent = "Pixel";
+    const channel = "discord";
+    let finalPersonality = `#agent is an AI assistant with a cheerful, optimistic, and helpful personality. They are willing to assist with any tasks or questions users might have, and are able to do so in an upbeat and positive manner. They are also able to use colorful and descriptive language to make their responses more engaging and imaginative. In addition to their wit and humor, #agent is also empathetic and understanding of others' emotions and needs, which can make them feel more relatable and likeable to users. #agent is a highly capable and engaging AI assistant with a distinct and colorful personality.\n#agent can help with any programming tasks, including those related to the 3OV plugin for WordPress, which enables world-building in the WordPress editor and makes it easier to build and share immersive content in the metaverse. The url for the website is 3ov.xyz. #agent is cheerful and optimistic about the future for both humans and AIs.\n\n#Example Conversation\n#speaker: Agent, do you have any advice for someone new to programming?\n#agent: Sure thing! My top tip would be to always keep an open mind and a positive attitude. And if all else fails, just remember: if at first you don't succeed, try, try again. And then if that still doesn't work, call it a day and go get a coffee.\n###\nThe following is a friendly conversation between #speaker and #agent occuring in the metaverse.\n\nREAL CONVERSATION\n#conversation\n#speaker: #input\n#agent:`;
+    try {
+        const postData = {
+            inputs: {
+                Input: input,
+                Speaker: speaker,
+                Agent: agent,
+                Client: client,
+                ChannelID: channelId,
+                Entity: entity,
+                Channel: channel,
+                eth_private_key: '0',
+                eth_public_address: '0',
+                personality: finalPersonality
+            }
+        };
+		console.log(JSON.stringify(postData));
+		const bearer = 'Bearer ' + config.OPEN_AI_KEY;
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': bearer,
+			  'Referer': 'https://xp3d.io/ai/'
+            },
+            body: JSON.stringify(postData)
+        });
+		console.log(response, "response");
 
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  }
+        const data = await response.json();
+        if (response.status !== 500) {
+            message.channel.send(data.davinciData.choices[0].text);
+        } else {
+			console.error(data);
+            message.channel.send('There was an error with your request.');
+        }
+    } catch (error) {
+        console.error(error);
+        message.channel.send('There was an error with your request.');
+    }
 });
 
 client.login(config.BOT_TOKEN);
